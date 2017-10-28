bd31333b1a12d556817b0365338cbec833ab6607
QFLaoXu <854560715@qq.com>
2017-10-28 20:25:23 +0800

    重构代码，新增设置边框圆角参数

--- src/JS/XToast.js
+++ src/JS/XToast.js
@@ -1,19 +1,21 @@
 (function (document, owner) {
 
     var dMess = document.createElement("div");
-    var clientWidth = document.documentElement.clientWidth;
+    // var clientWidth = document.documentElement.clientWidth;
     var clientHeight = document.documentElement.clientHeight;
 
     var getTimeoutFirst = 0;
     var getTimeoutSecond = 0;
 
-    var boxWidth = 0;
-    var boxHeight = 0;
-    var fontSize = 0;
-    var fontColor = "";
-    var unit = "";
-    var windowTop = "null";
-    var windowBottom = 0;
+    // ------------------
+    var mBoxWidth = 0;
+    var mBoxHeight = 0;
+    var mFontSize = 0;
+    var mFontColor = "";
+    var mUnit = "";
+    var mWindowTop = "null";
+    var mWindowBottom = 0;
+    var mBorderRadius = 0;
 
     /* init view */
     createView(200, 40, 14, "#FFF", "px", 100);
@@ -27,48 +29,73 @@
      * @param  {[string]} textColor [description] 设置字体颜色
      * @param  {[string]} unit      [description] 设置单位(px/em/rem...)
      * @param  {[number]} bottom    [description] 设置距离窗口下部分的距离
+     * @param  {[number]} borderRadius [description] 设置边框圆角
      * @return {[undefined]}        [description]
      */
-    owner.initView = function (bWidth, bHeight, textSize, textColor, unit, bottom) {
+    owner.initView = function (bWidth, bHeight, textSize, textColor, unit, bottom, borderRadius) {
         var paramLength = arguments.length;
         if (paramLength == 0) {
-            windowBottom = 100;
+            mWindowBottom = 100;
             createView()
         }
         if (paramLength == 1) {
             if (isObject(arguments[0])) {
                 // this is object
-                createView(arguments[0].width, arguments[0].height, arguments[0].fontSize, arguments[0].color, arguments[0].unit, arguments[0].buttom)
+                createView(arguments[0].width, arguments[0].height, arguments[0].fontSize, arguments[0].color, arguments[0].unit, arguments[0].buttom, arguments[0].borderRadius)
             } else {
-                createView2ChangeY(arguments[0])
+                setWindowTopOrBottom(arguments[0])
+                createView();
             }
 
-        } else if (paramLength == 2) {
-            createView2ChangeY(arguments[0], arguments[1])
+        } else if (paramLength == 2 || paramLength == 3) {
+            setWindowTopOrBottom(arguments[0]);
+            setFontColor(arguments[1]);
+            setBorderRadius(arguments[2]);
+            createView()
         } else {
-            createView(bWidth, bHeight, textSize, textColor, unit, bottom)
+            createView(bWidth, bHeight, textSize, textColor, unit, bottom, borderRadius)
         }
+    }
 
+
+    /**
+     * 设置边框弧度
+     * @param borderRadius {[number]}
+     */
+    function setBorderRadius(borderRadius) {
+        mBorderRadius = borderRadius;
     }
 
     /**
-     * 创建视图并修改在Y轴上的位置
-     * @param  {[number or string]} y  [description] Y轴方向的大小，"top":最上方，"center":中间位置,"bottom":最下方
-     * @param  {[string]} color        [description] 字体颜色值
-     * @return {[undefined]}           [description]
+     * 设置字体颜色
+     * @param color {[String]}
      */
-    function createView2ChangeY(y, color) {
+    function setFontColor(color) {
+        if (typeof color == undefined) {
+            color = "#FFF";
+            if (typeof color != "string") {
+                throw new Error("Please set color string value!");
+            }
+        }
+        mFontColor = color;
+    }
+
+    /**
+     * 设置XToast在屏幕上的显示位置
+     * @param y {[number]}
+     */
+    function setWindowTopOrBottom(y) {
         if (typeof y == "string") {
             y = y.toLocaleLowerCase();
             if (y == "top") {
-                windowTop = 0;
-                windowBottom = "null";
+                mWindowTop = 0;
+                mWindowBottom = "null";
             } else if (y == "center") {
-                windowTop = clientHeight / 2;
-                windowBottom = "null";
+                mWindowTop = clientHeight / 2;
+                mWindowBottom = "null";
             } else if (y == "bottom") {
-                windowTop = "null";
-                windowBottom = 0;
+                mWindowTop = "null";
+                mWindowBottom = 0;
             } else {
                 y = parseFloat(y);
                 if (isNaN(y)) {
@@ -78,81 +105,79 @@
         }
         if (typeof y == "number") {
             if (y <= 0) {
-                windowTop = 0;
-                windowBottom = "null";
+                mWindowTop = 0;
+                mWindowBottom = "null";
             } else if (y >= clientHeight) {
-                windowTop = "null";
-                windowBottom = 0;
+                mWindowTop = "null";
+                mWindowBottom = 0;
             } else {
-                windowTop = y;
-                windowBottom = "null"
-                //windowBottom = y;
+                mWindowTop = y;
+                mWindowBottom = "null"
+                //mWindowBottom = y;
             }
 
         }
-        if (typeof color == undefined) {
-            color = "#FFF";
-            if (typeof color != "string") {
-                throw new Error("Please set color string value!");
-            }
-        }
-
-        fontColor = color;
-        createView()
     }
 
     /*创建视图*/
-    function createView(bWidth, bHeight, textSize, textColor, unit, bottom) {
+    function createView(bWidth, bHeight, textSize, textColor, unit, bottom, borderRadius) {
         if (bottom == undefined) {
-            windowBottom = windowBottom || 1;
+            mWindowBottom = mWindowBottom || 1;
         } else {
-            windowBottom = bottom || 1;
+            mWindowBottom = bottom || 1;
         }
+
         if (textColor == undefined) {
-            fontColor = "" + fontColor
+            mFontColor = "" + mFontColor
         } else {
-            fontColor = "" + textColor
+            mFontColor = "" + textColor
         }
 
-        boxWidth = parseFloat(bWidth) || 200;
-        boxHeight = parseFloat(bHeight) || 40;
-        fontSize = parseFloat(textSize) || 16;
+        mBoxWidth = parseFloat(bWidth) || 200;
+        mBoxHeight = parseFloat(bHeight) || 40;
+        mFontSize = parseFloat(textSize) || 16;
 
-        unit = getUnit(unit + "");
+        if (borderRadius == undefined) {
+            mBorderRadius = parseFloat(mBorderRadius) || (mBoxWidth / 2);
+        } else {
+            mBorderRadius = parseFloat(borderRadius) || (mBoxWidth / 2);
+        }
 
-        windowTop = windowTop || 1;
+
+        mUnit = getUnit(unit + "");
+
+        mWindowTop = mWindowTop || 1;
 
         /** 初始化CSS样式 */
-        dMess.style.width = boxWidth + "" + unit;
-        dMess.style.height = boxHeight + "" + unit;
+        dMess.style.width = mBoxWidth + "" + mUnit;
+        dMess.style.height = mBoxHeight + "" + mUnit;
         dMess.style.background = "rgba(0,0,0,0.5)";
         dMess.style.position = "fixed";
         dMess.style.left = "50%";
-        dMess.style.marginLeft = (-boxWidth / 2) + "" + unit
+        dMess.style.marginLeft = (-mBoxWidth / 2) + "" + mUnit
 
-        dMess.style.borderRadius = (boxHeight / 2) + "" + unit;
-        dMess.style.font = (+fontSize) + "px/" + (+boxHeight) + unit + " '楷体','宋体'";
+        dMess.style.borderRadius = mBorderRadius + "" + mUnit;
+        dMess.style.font = (+mFontSize) + "px/" + (+mBoxHeight) + mUnit + " '楷体','宋体'";
         dMess.style.textAlign = 'center';
-        dMess.style.color = fontColor + "";
+        dMess.style.color = mFontColor + "";
 
         dMess.id = "toastInfo";
         dMess.style.display = "none";
-
-        if (typeof windowBottom == "number") {
-            dMess.style.bottom = windowBottom + "" + unit;
-        } else if (typeof windowTop == "number") {
-            dMess.style.top = windowTop + "" + unit;
+        if (typeof mWindowBottom == "number") {
+            dMess.style.bottom = mWindowBottom + "" + mUnit;
+        } else if (typeof mWindowTop == "number") {
+            dMess.style.top = mWindowTop + "" + mUnit;
         }
 
         document.body.appendChild(dMess);
     }
 
     /*获取当前的单位（px/em/rem）*/
-    function getUnit(unit) {
-        unit = unit || "px";
+    function getUnit(mUnit) {
+        mUnit = mUnit || "px";
         var str = "";
 
-        switch (unit) {
+        switch (mUnit) {
             case "em":
                 str = "em";
                 break;
@@ -172,7 +197,7 @@
      * @return {Boolean}     [description] true:Object类型 <br>
      *                                     false:表示为其他类型
      */
-    function isObject(obj){
+    function isObject(obj) {
         return Object.prototype.toString.call(obj) == '[object Object]';
     }
 
